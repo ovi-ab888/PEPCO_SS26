dadb0f5e08abaa3f5ea450c430453c0465bad6a6
ovi-ab888 <oviabdulohab@gmail.com>
2025-08-21 10:20:28 +0600

    Update pepco_SS26.py

--- pepco_SS26.py
+++ pepco_SS26.py
@@ -490,9 +490,11 @@ def process_pepco_pdf(uploaded_pdf, extra_order_ids: str | None = None):
                 key="pepco_washing_code"
             )
 
-            df = pd.DataFrame(result_data)
+                        df = pd.DataFrame(result_data)
+
             if extra_order_ids:
-            df['Order_ID'] = df['Order_ID'].astype(str) + "+" + extra_order_ids
+                df['Order_ID'] = df['Order_ID'].astype(str) + "+" + extra_order_ids
+
             df['Dept'] = df['Item_classification'].apply(get_dept_value)
             df['Cotton'] = cotton_value
             df['Collection'] = df.apply(lambda row: modify_collection(row['Collection'], row['Item_classification']), axis=1)
@@ -608,6 +610,7 @@ if __name__ == "__main__":
 
 st.markdown("---")
 st.caption("This app developed by Ovi")
+
 
 
 
